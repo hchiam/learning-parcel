174502d92b0522aa6c4349e8eda95264ebb4e4be
hchiam <hchiam@users.noreply.github.com>
2019-11-03 21:39:12 -0500

    add lodash to example

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // import another component
 import main from './main';
+import _ from 'lodash'; // this library will be automatically added to node_modules, package.json, and package-lock.json
 
 main();
+
+let animals = ['apple', 'banana', 'coconut', 'durian'];
+
+if (!isEmpty(animals)) {
+  animals = map(animals, animal => animal.toUpperCase());
+}
+
+alert('All uppercase: ' + animals.join(', '));
